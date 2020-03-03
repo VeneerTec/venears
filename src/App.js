3c7bdb4bed29977a141b3503fca8e1e384f80fcd
VeneerTec <ledinger@VeneerTech.com>
2020-03-03 14:39:12 -0500

    update

--- src/App.js
+++ src/App.js
@@ -3,68 +3,73 @@ import './App.css';
 
 
 class App extends React.Component {
+  
   spanish() {
-    var x = document.getElementById("langButton").value
-    console.log("Success!")
-    x.innerHTML = "Success!"
+
   }
-  render () {
+  enterEmail() {
+    var emailLink = ("https://airform.io/" + document.getElementById("inputEmail").value)
+    document.getElementsByTagName("form")[0].setAttribute("action", emailLink)
+  }
+
+  render() {
     return (
       <div>
-      <body>
-        <div class="header">
-          <div class="langSelect">
-            <input id="langButton" class="langButton" type="button" onClick={this.spanish} value="Espa&ntilde;ol"></input>
+          <div class="header">
+            <div class="langSelect">
+              <input id="langButton" class="langButton" type="button" onClick={this.spanish} value="Espa&ntilde;ol"></input>
+            </div>
+            <h1>Veneer Tech Employer's Accident Reporting System</h1>
+            <p class="p1">BETA VERSION</p>
           </div>
-          <h1>Veneer Tech Employer's Accident Reporting System</h1>
-          <p class="p1">BETA VERSION</p>
-        </div>
-        <table class="devTable">
-          <tbody>
-            <tr>
-              <td>DEV</td>
-              <td>TOOLS</td>
-            </tr>
-            <tr>
-              <td>Where to send email</td>
-              <td><input class="wrapper" type="text" name="Email"></input></td>
-            </tr>
-          </tbody>
-        </table>
-        <form method="post" action="https://airform.io/{email}">
-          <table class="table">
+          <table class="devTable">
             <tbody>
               <tr>
-                <td>What is your name?</td>
-                <td><input class="wrapper" type="text" name="name" /></td>
+                <td>DEV</td>
+                <td>TOOLS</td>
               </tr>
               <tr>
-                <td>What is your quest?</td>
-                <td><input class="wrapper" type="text" name="quest" /></td>
-              </tr>
-              <tr>
-                <td>What is your favorite color?</td>
-                <td><input class="wrapper" type="text" name="color" /></td>
+                <td>Where to send email</td>
+                <td><input class="wrapper" type="text" name="Email" id="inputEmail"></input></td>
               </tr>
             </tbody>
           </table>
-          <textarea class="wrapper" name="message"></textarea>
-          <button class="button">Send</button>
+          <form method="post" id="formPost">
+            <table class="table">
+              <tbody>
+                <tr>
+                  <td>What is your name?</td>
+                  <td><input class="wrapper" type="text" name="name" /></td>
+                </tr>
+                <tr>
+                  <td>What is your quest?</td>
+                  <td><input class="wrapper" type="text" name="quest" /></td>
+                </tr>
+                <tr>
+                  <td>What is your favorite color?</td>
+                  <td><input class="wrapper" type="text" name="color" /></td>
+                </tr>
+              </tbody>
+            </table>
+            <textarea class="wrapper" name="message"></textarea>
+            <button class="button" id="sendButton" onClick={this.enterEmail}>Send</button>
 
-        </form>
-      </body>
-      <footer>
-        <p>&copy; 2020 Möhring Group &emsp;</p>
-      </footer>
-    </div>
+          </form>
+        <footer>
+          <p>&copy; 2020 Möhring Group &emsp;</p>
+        </footer>
+      </div>
     )
   }
 }
 
 class input extends React.Component {
-  render () {
+  render() {
     return (
-      <input id="langButton" class="langButton" type="button" onClick={this.spanish} value="Espa&ntilde;ol"></input>
+      <div>
+        <input id="langButton" class="langButton" type="button" onClick={this.spanish} value="Espa&ntilde;ol"></input>
+        <button class="button" id="sendButton" onClick={this.sendButton}>Send</button>
+      </div>
     )
   }
 }
