f45d80e437f92e5f328eedefbbcde20ad896bd41
VeneerTec <ledinger@VeneerTech.com>
2020-03-03 12:58:23 -0500

    master

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <body>
+        <div class="header">
+          <div class="langSelect">
+            <input id="langButton" class="langButton" type="button" onclick="langBtn()" value="Espa&ntilde;ol"></input>
+          </div>
+          <h1>Veneer Tech Employer's Accident Reporting System</h1>
+          <p1>BETA VERSION</p1>
+        </div>
+        <form method="post" action="https://airform.io/ledinger@veneertech.com">
+          <table class="table">
+            <tbody>
+              <tr>
+                <td>What is your name?</td>
+                <td><input class="wrapper" type="text" name="name" /></td>
+              </tr>
+              <tr>
+                <td>What is your quest?</td>
+                <td><input class="wrapper" type="text" name="quest" /></td>
+              </tr>
+              <tr>
+                <td>What is your favorite color?</td>
+                <td><input class="wrapper" type="text" name="color" /></td>
+              </tr>
+            </tbody>
+          </table>
+          <textarea class="wrapper" name="message"></textarea>
+          <button class="button">Send</button>
+
+        </form>
+      </body>
+      <footer>
+        <p>&copy; 2020 Möhring Group &emsp;</p>
+      </footer>
     </div>
   );
 }
 
+// https://stackoverflow.com/questions/37295377/how-to-navigate-from-one-page-to-another-in-react-js
 export default App;
