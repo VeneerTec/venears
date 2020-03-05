eb829edd9b9c471d6d748cd4b60ed0aed1b0bcc4
VeneerTec <ledinger@VeneerTech.com>
2020-03-05 08:29:28 -0500

    commit

--- src/App2.js
+++ src/App2.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 
 
-
 class App extends React.Component {
 
     spanish() {
--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 //import App from './App';
 import App2 from './App2'
+import EmployeeTemplate from './EmployeeTemplate'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App2 />, document.getElementById('root'));
+ReactDOM.render(<EmployeeTemplate />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
